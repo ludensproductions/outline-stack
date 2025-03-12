719ab7285666645879b2b6f37aa71b81ab3f9e31
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-11 19:59:47 -0700

    feat: added powershell script for local testing
    feat: separated variables from docker commands

--- backup.sh
+++ backup.sh
@@ -1,10 +1,12 @@
 # Conseguir nombre del volumen a ojo, siempre viene siendo {nombre_del_stack_o_carpeta}_{nombre_volumen}
 # sudo docker volume ls
 
-# Extraer
-OUTLINE_VOLUME=outline-data && sudo docker run --rm -v "${OUTLINE_VOLUME}:/volume-data" -v "./${OUTLINE_VOLUME}:/backup-data" busybox sh -c 'cp -rv /volume-data/* /backup-data'
+OUTLINE_VOLUME="outline-stack_outline-data"
+POSTGRES_VOLUME="outline-stack_postgres-data"
 
-POSTGRES_VOLUME=postgres-data && sudo docker run --rm -v "${POSTGRES_VOLUME}:/volume-data" -v "./${POSTGRES_VOLUME}:/backup-data" busybox sh -c 'cp -rv /volume-data/* /backup-data'
+# Extraer
+sudo docker run --rm -v "${OUTLINE_VOLUME}:/volume-data" -v "./${OUTLINE_VOLUME}:/backup-data" busybox sh -c 'cp -rv /volume-data/* /backup-data'
+sudo docker run --rm -v "${POSTGRES_VOLUME}:/volume-data" -v "./${POSTGRES_VOLUME}:/backup-data" busybox sh -c 'cp -rv /volume-data/* /backup-data'
 
 # Borrar backups viejos con mas de 7 dias de antiguedad
 find ./backups -maxdepth 1 -type d -name 'backup_*' -mtime +7 -exec echo Deleting {} \;
@@ -16,5 +18,5 @@ BACKUP_DIR="./backups/${BACKUP_DIR_NAME}"
 mkdir ${BACKUP_DIR}
 
 # Mover datos del outline y postgres al directorio de backups
-sudo mv postgres-data/ "${BACKUP_DIR}"
-sudo mv outline-data/ "${BACKUP_DIR}"
+sudo mv "./${POSTGRES_VOLUME}/" "${BACKUP_DIR}"
+sudo mv "./${OUTLINE_VOLUME}/" "${BACKUP_DIR}"
