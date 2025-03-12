b68c923acb8476d004c8c892ad5ff2f6c1f57510
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-11 18:11:13 -0700

    feat: copy two volumes with the same script

--- backup.sh
+++ backup.sh
@@ -2,4 +2,12 @@
 # sudo docker volume ls
 
 # Extraer
-NOMBRE_VOLUMEN=nombre_volumen && sudo docker run --rm -v "${NOMBRE_VOLUMEN}:/volume-data" -v "./${NOMBRE_VOLUMEN}:/backup-data" busybox sh -c 'cp -rv /volume-data/* /backup-data'
+OUTLINE_VOLUME=outline-data && sudo docker run --rm \
+    -v "${OUTLINE_VOLUME}:/var/lib/outline/data" \
+    -v "./${OUTLINE_VOLUME}:/backup-data" \
+    busybox sh -c 'cp -rv /volume-data/* /backup-data'
+
+POSTGRES_VOLUME=postgres-data && sudo docker run --rm \
+    -v "${POSTGRES_VOLUME}:/var/lib/postgresql/data" \
+    -v "./${POSTGRES_VOLUME}:/backup-data" \
+    busybox sh -c 'cp -rv /volume-data/* /backup-data'
