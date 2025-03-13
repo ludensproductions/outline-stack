14a1d6bb37eb7312c0b56cc41fdcc63a7d0401ce
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-12 19:04:40 -0700

    refactor: cleaned code

--- git-restore.sh
+++ git-restore.sh
@@ -2,24 +2,29 @@
 OUTLINE_VOLUME="outline-stack_outline-data"
 POSTGRES_VOLUME="outline-stack_postgres-data"
 
-BACKUP_FOLDER="~/backups/outline-backup"
+BACKUP_FOLDER="$HOME/backups/outline-backup"
 
-OUTLINE_BACKUP="${BACKUP_FOLDER}/${OUTLINE_VOLUME}"
-POSTGRES_BACKUP="${BACKUP_FOLDER}/${POSTGRES_VOLUME}"
+OUTLINE_BACKUP=${BACKUP_FOLDER}/${OUTLINE_VOLUME}
+POSTGRES_BACKUP=${BACKUP_FOLDER}/${POSTGRES_VOLUME}
 
 # Check if the backup folder exists
 if [ ! -d ~/backups/outline-backup ]; then
-    echo "❌ Backup folder '$BACKUP_FOLDER' does not exist."
+    echo "Backup folder '$BACKUP_FOLDER' does not exist."
 else
-    # Run a temporary busybox container to restore the data into the volume
+    # Restore PostgreSQL
     sudo docker run --rm \
         -v "${POSTGRES_VOLUME}:/var/lib/postgresql/data" \
         -v "${POSTGRES_BACKUP}:/backup-data" \
         postgres:15 bash -c "cp -rv /backup-data/* /var/lib/postgresql/data && chown -R postgres:postgres /var/lib/postgresql/data"
 
+    echo "Restored PostgreSQL data"
+
+    # Restore Outline
     sudo docker run --rm \
         --user 1000:1000 \
         -v "${OUTLINE_VOLUME}:/var/lib/outline/data" \
         -v "${OUTLINE_BACKUP}:/backup-data" \
         docker.getoutline.com/outlinewiki/outline:0.82.0 sh -c "cp -rv /backup-data/* /var/lib/outline/data"
+
+    echo "Restored Outline data"
 fi
