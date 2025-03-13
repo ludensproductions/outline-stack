4d219e64876fb44ab50f09ff6cf823d44eb29d19
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-03-12 18:53:47 -0700

    fix: deleted exit in script

--- git-restore.sh
+++ git-restore.sh
@@ -8,19 +8,18 @@ OUTLINE_BACKUP="${BACKUP_FOLDER}/${OUTLINE_VOLUME}"
 POSTGRES_BACKUP="${BACKUP_FOLDER}/${POSTGRES_VOLUME}"
 
 # Check if the backup folder exists
-if [ ! -d "$BACKUP_FOLDER" ]; then
+if [ ! -d ~/backups/outline-backup ]; then
     echo "❌ Backup folder '$BACKUP_FOLDER' does not exist."
-    exit 1
+else
+    # Run a temporary busybox container to restore the data into the volume
+    sudo docker run --rm \
+        -v "${POSTGRES_VOLUME}:/var/lib/postgresql/data" \
+        -v "${POSTGRES_BACKUP}:/backup-data" \
+        postgres:15 bash -c "cp -rv /backup-data/* /var/lib/postgresql/data && chown -R postgres:postgres /var/lib/postgresql/data"
+
+    sudo docker run --rm \
+        --user 1000:1000 \
+        -v "${OUTLINE_VOLUME}:/var/lib/outline/data" \
+        -v "${OUTLINE_BACKUP}:/backup-data" \
+        docker.getoutline.com/outlinewiki/outline:0.82.0 sh -c "cp -rv /backup-data/* /var/lib/outline/data"
 fi
-
-# Run a temporary busybox container to restore the data into the volume
-sudo docker run --rm \
-    -v "${POSTGRES_VOLUME}:/var/lib/postgresql/data" \
-    -v "${POSTGRES_BACKUP}:/backup-data" \
-    postgres:15 bash -c "cp -rv /backup-data/* /var/lib/postgresql/data && chown -R postgres:postgres /var/lib/postgresql/data"
-
-sudo docker run --rm \
-    --user 1000:1000 \
-    -v "${OUTLINE_VOLUME}:/var/lib/outline/data" \
-    -v "${OUTLINE_BACKUP}:/backup-data" \
-    docker.getoutline.com/outlinewiki/outline:0.82.0 sh -c "cp -rv /backup-data/* /var/lib/outline/data"
