9f3ca632fc6105da70d694bdb4ed5d37bb512bc9
DanielParraE <daniel.parra216710@potros.itson.edu.mx>
2025-07-08 17:58:06 -0700

    feat: added discord notification function

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ load_dotenv()
 class OutlineAPIClient:
     def __init__(self):
         self.base_url = os.getenv("OUTLINE_URL")
+        self.disord_notification_url = os.getenv("DISCORD_NOTIFICATION_URL")
         self.api_key = os.getenv("OUTLINE_API_KEY")
+        self.docs_urls = None
+        self.target_notification_id = os.getenv("TARGET_NOTIFICATION_ID")
 
     def _generate_headers(self):
         headers = {
@@ -95,7 +98,8 @@ class OutlineAPIClient:
 
             time.sleep(0.1)
 
-        return list(set(docs_urls))
+        self.docs_urls = list(set(docs_urls))
+        return self.docs_urls
 
     def save_doc_ids_to_txt(self, file_path: Path, docs_list: list):
         try:
@@ -106,8 +110,57 @@ class OutlineAPIClient:
         except Exception as e:
             print(f"Failed to save to TXT: {e}")
 
+    def get_webhook_data(self):
+        headers = {
+            "Content-Type": "application/json"
+        }
+        files = {}
+
+        webhook_data = {
+            "url": self.disord_notification_url,
+            "files": files,
+            "headers": headers
+        }
+        return webhook_data
+
+    def send_message_to_webhook(
+        self,
+        content,
+        embed_title,
+        embed_description="",
+        embed_color=0x00FF00
+    ):
+        webhook_data = self.get_webhook_data()
+        payload = {
+            "content": content,
+            "embeds": [
+                {
+                    "title": embed_title,
+                    "description": embed_description,
+                    "color": embed_color
+                }
+            ]
+        }
+
+        requests.post(
+            webhook_data["url"],
+            headers=webhook_data["headers"],
+            data=json.dumps(payload),
+            files=webhook_data["files"]
+        )
+
+    def format_doc_ids_as_markdown(self):
+        markdown_list = "\n".join(f"{idx}. {doc_id}" for idx, doc_id in enumerate(self.docs_urls, start=1))
+        return markdown_list
+
 if __name__ == "__main__":
-    urls_txt = Path(__name__).parent.resolve() / "urls.txt"
+    # urls_txt = Path(__name__).parent.resolve() / "urls.txt"
     api_client = OutlineAPIClient()
-    docs_urls = api_client.fetch_unresolved_comment_doc_ids()
-    api_client.save_doc_ids_to_txt(urls_txt, docs_urls)
+
+    api_client.fetch_unresolved_comment_doc_ids()
+    if api_client.docs_urls:
+        api_client.send_message_to_webhook(
+            f"<@{api_client.target_notification_id}>",
+            "**Documents with unresolved comments**",
+            api_client.format_doc_ids_as_markdown(),
+        )
